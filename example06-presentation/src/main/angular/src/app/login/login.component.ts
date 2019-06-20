7540396c220e8a8b2cf06be9cb8018f44b0e7914
jan.v.d.horst <jan.v.d.horst@dirkmedia.de>
2019-06-20 23:22:31 +0200

    registration & login frontend functions

--- example06-presentation/src/main/angular/src/app/login/login.component.ts
+++ example06-presentation/src/main/angular/src/app/login/login.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { SessionAuthService } from '../auth/session-auth.service';
+import { AuthService } from '../auth/auth.service';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { AngularComponent } from '../angular/angular.component';
+import { AuthNewsService } from '../auth/auth-news.service';
+import { environment as env } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+import { JwtAuthService } from '../auth/jwt-auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
-export class NewLoginComponent implements OnInit {
+export class NewLoginComponent{
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {
+  @Output()
+  public username:string = "";
+  public password:string = "";
+  public firstname:string= "";
+  public lastname:string="";
+  public email:string="";
+
+  login = async (e: Event) => {
+    e.preventDefault();
+    const body = new HttpParams()
+    .set('username', this.username)
+    .set('password', this.password);
+
+    const headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
+    await this.http.post(`example06/rest/auth/user/login`, body.toString(), {headers, responseType: 'text'})
+    .subscribe(
+      data => alert('Login successful'),
+      error => { alert('Login failed'); console.log(error); }
+    );
   }
 
+  getBaseUrl(): string {
+    return `${env.apiUrl}/auth/jwt`
+  }
 }
--- example06-presentation/src/main/angular/src/app/registration/registration.component.ts
+++ example06-presentation/src/main/angular/src/app/registration/registration.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { SessionAuthService } from '../auth/session-auth.service';
+import { AuthService } from '../auth/auth.service';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { AngularComponent } from '../angular/angular.component';
+import { AuthNewsService } from '../auth/auth-news.service';
+import { environment as env } from '../../environments/environment';
+import { map, tap, catchError } from 'rxjs/operators';
+import { JwtAuthService } from '../auth/jwt-auth.service';
 
 @Component({
-  selector: 'app-registration',
+  selector: 'registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.sass']
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent{
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {
+  @Output()
+  public username:string = "";
+  public password:string = "";
+  public firstname:string= "";
+  public lastname:string="";
+  public email:string="";
+
+  register = async (e: Event) => {
+    e.preventDefault();
+    const body = new HttpParams()
+    .set('username', this.username)
+    .set('password', this.password)
+    .set('firstname', this.firstname)
+    .set('lastname', this.lastname)
+    .set('email', this.email);
+
+    const headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
+    await this.http.post(`example06/rest/auth/user/register`, body.toString(), {headers, responseType: 'text'})
+      .subscribe(
+        data => alert('Registered successfully'),
+        error => alert('OOPS ' + error)
+      );
   }
 
+  getBaseUrl(): string {
+    return `${env.apiUrl}/auth/jwt`
+  }
 }
