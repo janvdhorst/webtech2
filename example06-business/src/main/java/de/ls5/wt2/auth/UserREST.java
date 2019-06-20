876edea1579f60ab93a5a5fc0c86a7a4cd00c071
jan.v.d.horst <jan.v.d.horst@dirkmedia.de>
2019-06-20 23:22:08 +0200

    new api for authentication & shiro rule

--- example06-business/src/main/java/de/ls5/wt2/auth/UserREST.java
+++ example06-business/src/main/java/de/ls5/wt2/auth/UserREST.java
@@ -1,27 +1,107 @@
 package de.ls5.wt2.auth;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import de.ls5.wt2.conf.auth.permission.ViewFirstFiveNewsItemsPermission;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.Permission;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
+import javax.transaction.Transactional;
+import javax.ws.rs.FormParam;
+import java.security.*;
+import java.nio.charset.StandardCharsets;
+import de.ls5.wt2.DBUser;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
-@Path("/auth/{a:session|basic|jwt}/profile")
+@Path("/auth/user")
+@Transactional
 public class UserREST {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response getProfile() {
+    @PersistenceContext
+    private EntityManager entityManager;
 
-        final Subject subject = SecurityUtils.getSubject();
+    @POST
+    @Path("login")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("text/plain")
+    public Response loginUser(@FormParam("username") String username, @FormParam("password") String password) {
+        final CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+        final CriteriaQuery<DBUser> query = builder.createQuery(DBUser.class);
+        final Root<DBUser> from = query.from(DBUser.class);
+        query.select(from);
+        //query all users from DB
+        final List<DBUser> result = this.entityManager.createQuery(query).getResultList();
+        DBUser[] users = new DBUser[result.size()];
+        users = result.toArray(users);
+        for(int i=0; i<users.length; i++) {
+          if(users[i].getUsername().equals(username)) {
+            try {
+            //matching user found - initialize MD5 hashing (md5 does not use salts)
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            byte[] newPw = md.digest();
 
-        if (!subject.isAuthenticated()) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
+            //compare hashes
+            StringBuilder sb = new StringBuilder();
+            for(int x=0; x<newPw.length; x++) {
+              sb.append(Integer.toString((newPw[x] & 0xff) + 0x100, 16).substring(1));
+            }
+            if(sb.toString().equals(users[i].getPassword().toString())) {
+              String JWTToken = "";
+              //Login successful
+              /* 
+              
+                TODO here: generate and send JWT token
+
+               */
+              return Response.ok(JWTToken).build();
+            }
+            }catch(NoSuchAlgorithmException e) {
+              return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build(); 
+            }
+          }
+        }    
+        return Response.status(Response.Status.UNAUTHORIZED).build(); 
+    }
+
+    @POST
+    @Path("register")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("text/plain")
+    public Response registerUser(@FormParam("username") String username, @FormParam("password") String password, @FormParam("firstname") String firstname,
+      @FormParam("lastname") String lastname, @FormParam("email") String email) {
+        final DBUser user = new DBUser();
+        user.setUsername(username);
+        user.setFirstname(firstname);
+        user.setLastname(lastname);
+        user.setEmail(email);
+        try {
+          MessageDigest md = MessageDigest.getInstance("MD5");
+          md.update(password.getBytes());
+          byte[] hashedPassword = md.digest();
+          StringBuilder sb = new StringBuilder();
+          for(int i=0; i<hashedPassword.length; i++) {
+            sb.append(Integer.toString((hashedPassword[i] & 0xff) + 0x100, 16).substring(1));
+          }
+          user.setPassword(sb.toString());
+          this.entityManager.persist(user);   
+          return Response.ok(user).build();     
+        }catch(NoSuchAlgorithmException e) {
+          return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
-
-        return Response.ok(subject.getPrincipal().toString()).build();
+        
     }
 }
