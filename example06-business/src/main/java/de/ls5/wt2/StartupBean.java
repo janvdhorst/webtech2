feaf1d3042bfd8b13994f70d069857b19709a065
abdelkefi <abdelkefi@DE-ADN-2SP8M12.adesso.local>
2019-06-24 22:31:57 +0200

    some quotes added to Start up and Peter Griffin as default user to test the buttons

--- example06-business/src/main/java/de/ls5/wt2/StartupBean.java
+++ example06-business/src/main/java/de/ls5/wt2/StartupBean.java
@@ -23,13 +23,41 @@ public class StartupBean {
 
         // only initialize once
         if (firstNewsItem == null) {
-            final DBNews news = new DBNews();
+            final DBNews news1 = new DBNews();
+            final DBNews news2 = new DBNews();
+            final DBNews news3 = new DBNews();
+            final DBNews news4 = new DBNews();
+            final DBNews news5 = new DBNews();
 
-            news.setHeadline("Startup");
-            news.setContent("Startup Bean successfully executed");
-            news.setPublishedOn(new Date());
+            news1.setHeadline("Satchel Paige");
+            news1.setContent("Work like you don't need the money. Love like you've never been hurt. Dance like nobody's watching.");
+            news1.setPublishedOn(new Date());
+            
+            news2.setHeadline("Barney Stinson");
+            news2.setContent("Believe it or not, I was not always as awesome as I am today");
+            news2.setPublishedOn(new Date());
+            
+            news3.setHeadline("Alexander Milne");
+            news3.setContent("People say nothing is impossible, but i do nothing everyday!");
+            news3.setPublishedOn(new Date());
+            
+            news3.setHeadline("Peter Griffin");
+            news3.setContent("I have an idea so smart that my head would explode if I even began to know what I was talking about.");
+            news3.setPublishedOn(new Date());
+            
+            news4.setHeadline("Abraham Lincoln");
+            news4.setContent("Better to remain silent and be thought a fool than to speak out and remove all doubt.");
+            news4.setPublishedOn(new Date());
+            
+            news5.setHeadline("Peter Griffin");
+            news5.setContent("Love is like a fart. if you have to force it, it's probably crap.");
+            news5.setPublishedOn(new Date());
 
-            this.entityManager.persist(news);
+            this.entityManager.persist(news1);
+            this.entityManager.persist(news2);
+            this.entityManager.persist(news3);
+            this.entityManager.persist(news4);
+            this.entityManager.persist(news5);
         }
     }
 
